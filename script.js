1c03dea7f32b966bbee0484884bba5c82c25647b
Willem Plug <PLUG434330@exactsoftware.com>
2023-09-22 10:11:25 +0200

    First basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementById('arrayMessage').innerHTML = parsed;
+const things = ["1", "2", "3"];
+
+function output(){
+    const randomIndex = Math.floor(Math.random() * things.length);
+    const random = things[randomIndex];
+
+    document.getElementById('arrayMessage').innerHTML = random;
+}
