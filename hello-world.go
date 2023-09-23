1099e872f4e4a08237e439461ba43e02e2a4c375
whplug <whplug@gmail.com>
2023-09-23 11:05:49 +0200

    Trying to implement a backend

---
+++ hello-world.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloWorldPage)
+	fmt.Println("Starting server on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
