7a81e209dd7fd03edd966acbc2fdf32d36acc96d
Willem Plug <PLUG434330@exactsoftware.com>
2023-09-25 11:29:23 +0200

    I dont know what I am doing anymore

--- go.mod
+++
@@ -1,3 +0,0 @@
-module lang-app
-
-go 1.21.1
--- hello-world.go
+++ hello-world.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	//"fmt"
+	"html/template"
 	"net/http"
 )
 
+render func(w http.ResponseWriter, tmpl string, r *http.Request) {
+	var tmpls = template.Must(template.ParseFiles("index/index.html", "index/2.html"))
+	tmpls.Execute(w, nil)
+}
+
 func main() {
-	fs := http.FileServer(http.Dir("./index"))
-	http.Handle("/", fs)
+
+
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", h1)
+	http.ListenAndServe(":8080", nil)
 }
